cd7d4d69a62107cf6c70bfef228359476c3cb676
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-12-15 14:35:58 -0700

    Initial commit

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import nox
+
+
+@nox.session(reuse_venv=True)
+def formatting(session: nox.Session) -> None:
+    session.run("poetry", "-q", "shell", external=True)
+    session.run("poetry", "install", external=True)
+    session.run("len8")
+    session.run("isort", ".")
+    session.run("black", ".", "--check")
+
+
+@nox.session(reuse_venv=True)
+def types(session: nox.Session) -> None:
+    session.run("poetry", "-q", "shell", external=True)
+    session.run("poetry", "install", external=True)
+    session.run("mypy", ".")
+    session.run("pyright")
