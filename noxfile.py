c77f2371c9b47095fa246a89ed36955de7019ac0
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-12-15 19:40:32 -0700

    Only install deps necessary per nox session

--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,27 @@
+import toml
 import nox
+
+
+def get_dependencies() -> dict[str, str]:
+    with open("pyproject.toml") as f:
+        data = toml.loads(f.read())["tool"]["poetry"]["dev-dependencies"]
+
+    return dict((k, f"{k}{v}") for k, v in data.items())
+
+
+DEPS = get_dependencies()
 
 
 @nox.session(reuse_venv=True)
 def formatting(session: nox.Session) -> None:
-    session.run("poetry", "-q", "shell", external=True)
-    session.run("poetry", "install", external=True)
+    session.install("-U", DEPS["len8"], DEPS["isort"], DEPS["black"])
     session.run("len8")
-    session.run("isort", ".")
-    session.run("black", ".", "--check")
+    session.run("isort", "chess")
+    session.run("black", "chess", "--check")
 
 
 @nox.session(reuse_venv=True)
 def types(session: nox.Session) -> None:
-    session.run("poetry", "-q", "shell", external=True)
-    session.run("poetry", "install", external=True)
-    session.run("mypy", ".")
+    session.install("-U", DEPS["mypy"], DEPS["pyright"])
+    session.run("mypy", "chess")
     session.run("pyright")
